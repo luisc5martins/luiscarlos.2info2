6f4c995ec8de487156c2ee88131fb3488f881914
luisc5martins <luisc.martinsjunior5@gmail.com>
2023-03-13 11:56:23 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const contador = ref(0)
+
+function incrementarContador() {
+  contador.value++
+}
+function decrementarContador() {
+  contador.value--
+}
+
+onMounted(() => {
+  console.log(`O valor inicial do contador é ${contador.value}.`)
+})
+</script>
+
+<template>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <p>Valor do contador é: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>

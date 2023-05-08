e9d41132ef69e954b81d62a805509fa711ff5e2d
luisc5martins <luisc.martinsjunior5@gmail.com>
2023-05-08 11:54:36 -0300

    atualizações 08/05

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,137 @@
 <script setup>
 
+import { ref } from 'vue'
+
+const name = ref('')
+const email = ref('')
+const senha = ref('')
+const confirmasenha = ref('')
+const datanascimento = ref('')
+const endereco = ref('')
+const cidade = ref('')
+const estado = ref('')
+
 </script>
 
 <template>
-  <label name="nome">Nome:</label>
-  <input type="text" name="nome" placeholder="Digite seu nome">
-  <label name="email">Email:</label>
-  <input type="email" name="email" placeholder="Digite seu Email">
-  <label name="senha">Senha:</label>
-  <input type="password" name="senha" placeholder="Digite sua Senha">
-  <label name="confirmasenha">Confirme sua senha:</label>
-  <input type="password" name="confirmasenha" placeholder="Digite sua senha novamente">
-  <label name="datanascimento">Data de nascimento</label>
-  <input type="date" name="datanascimento" placeholder="Digite sua data de nascimento">
-  <label name="endereco">Endereço:</label>
-  <input type="text" name="endereco" placeholder="Digite seu endereço">
-  <label name="cidade">Cidade:</label>
-  <input type="string" name="cidade" placeholder="Digite sua cidade">
-  <label name="estado">Estado</label>
-  <select name="Estado">
-    <option value="vazio">selecione seu estado</option>
-    <option value="ac">AC</option>
-    <option value="al">AL</option>
-    <option value="ap">AP</option>
-    <option value="am">AM</option>
-    <option value="ba">BA</option>
-    <option value="ce">CE</option>
-    <option value="df">DF</option>
-    <option value="es">ES</option>
-    <option value="go">GO</option>
-    <option value="ma">MA</option>
-    <option value="mt">MT</option>
-    <option value="ms">MS</option>
-    <option value="mg">MG</option>
-    <option value="pa">PA</option>
-    <option value="pb">PB</option>
-    <option value="pe">PE</option>
-    <option value="pi">PI</option>
-    <option value="pr">PR</option>
-    <option value="rj">RJ</option>
-    <option value="rn">RN</option>
-    <option value="rs">RS</option>
-    <option value="ro">RO</option>
-    <option value="rr">RR</option>
-    <option value="sc">SC</option>
-    <option value="sp">SP</option>
-    <option value="se">SE</option>
-    <option value="to">TO</option>
-  </select>
-
-  <!-- <div class="informacoesadicionais"> -->
-  <label name="hobbie">Hobbie:</label>
-  <input type="text" name="hobbie" placeholder="Digite seu hobbie">
-  <div>
-    <label name="linguagem">Linguagens de Programação:</label> <br />
-  </div>
-  <div></div>
-  <!-- <div class="langs"> -->
+  <form class="info" @submit.prevent="enviar()">
+    <label name="name">Nome:</label>
+    <br>
+    <input type="text" v-model="name" placeholder="Digite seu nome">
+    <br>
+    <label name="email">Email:</label>
+    <br>
+    <input type="email" v-model="email" placeholder="Digite seu Email">
+    <br>
+    <label name="senha">Senha:</label>
+    <br>
+    <input type="password" v-model="senha" placeholder="Digite sua Senha">
+    <br>
+    <label name="confirmasenha">Confirme sua senha:</label>
+    <br>
+    <input type="password" v-model="confirmasenha" placeholder="Digite sua senha novamente">
+    <br>
+    <label name="datanascimento">Data de nascimento</label>
+    <br>
+    <input type="date" v-model="datanascimento" placeholder="Digite sua data de nascimento">
+    <br>
+    <label name="endereco">Endereço:</label>
+    <br>
+    <input type="text" v-model="endereco" placeholder="Digite seu endereço">
+    <br>
+    <label name="cidade">Cidade:</label>
+    <br>
+    <input type="string" v-model="cidade" placeholder="Digite sua cidade">
+    <br>
+    <label name="estado">Estado</label>
+    <br>
+    <select v-model="estado">
+      <option value="vazio">selecione seu estado</option>
+      <option value="ac">AC</option>
+      <option value="al">AL</option>
+      <option value="ap">AP</option>
+      <option value="am">AM</option>
+      <option value="ba">BA</option>
+      <option value="ce">CE</option>
+      <option value="df">DF</option>
+      <option value="es">ES</option>
+      <option value="go">GO</option>
+      <option value="ma">MA</option>
+      <option value="mt">MT</option>
+      <option value="ms">MS</option>
+      <option value="mg">MG</option>
+      <option value="pa">PA</option>
+      <option value="pb">PB</option>
+      <option value="pe">PE</option>
+      <option value="pi">PI</option>
+      <option value="pr">PR</option>
+      <option value="rj">RJ</option>
+      <option value="rn">RN</option>
+      <option value="rs">RS</option>
+      <option value="ro">RO</option>
+      <option value="rr">RR</option>
+      <option value="sc">SC</option>
+      <option value="sp">SP</option>
+      <option value="se">SE</option>
+      <option value="to">TO</option>
+    </select>
+    <br>
+    <label name="hobbie">Hobbie:</label>
+    <br>
+    <input type="text" name="hobbie" placeholder="Digite seu hobbie">
+    <br>
+    <label name="linguagem">Linguagens de Programação:</label>
+    <br>
     <input type="checkbox" v-model="linguagemdepgr" value="languagecode">
     <label for="linguagemdepgr">C++</label>
+    <br>
     <input type="checkbox" v-model="linguagemdepgr" value="languagecode">
     <label for="linguagemdepgr">JavaScript</label>
+    <br>
     <input type="checkbox" v-model="linguagemdepgr" value="languagecode">
     <label for="linguagemdepgr">Java</label>
+    <br>
     <input type="checkbox" v-model="linguagemdepgr" value="languagecode">
     <label for="linguagemdepgr">Python</label>
-  <!-- </div> -->
-<!-- </div> -->
+    <br>
+    <label name="hobbie">Biografia:</label>
+    <br>
+    <input type="text" name="biografia" placeholder="Escreva sua biografia">
 
-<label name="hobbie">Hobbie:</label>
-  <input type="text" name="hobbie" placeholder="Digite seu hobbie">
+    <button type="submit" @click="aceitar = !aceitar">Submit</button>
 
+    <div v-if="aceitar" id="div">
+      <img :src="user.avatar" />
+      <p>{{ nome }}</p>
+      <p>{{ email }}</p>
+      <p>{{ senha }}</p>
+      <p>{{ data }}</p>
+      <p>{{ endereço }}</p>
+      <p>{{ cidade }}</p>
+      <p>{{ estado }}</p>
+      <p>{{ hobbies }}</p>
+      <p>{{ linguagemProg }}</p>
+      <p>{{ biografia }}</p>
+    </div>
+
+  </form>
 </template>
 
 <style scoped>
-button {
-  font-weight: bold;
+form {
+  background-color: rgb(0, 20, 110);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
 }
 
+#div {
+  background-color: rgba(35, 12, 22, 1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+  text-align: center;
+  margin: 0px 0px 0px 50px;
+  height: fit-content;
+}
 </style>
